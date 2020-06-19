259de6819c3bd472a1555451b23a5bfeeb1612e5
anqi <18742024663@163.com>
2020-06-19 09:47:25 +0800

    封装四个view和Layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,14 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper=styled.div`
-  height:100vh;
-  display:flex;
-  flex-direction: column;
-`
-const Main=styled.div`
-  flex-grow:1;
-`
+import Money from 'views/Money';
+import Tags from 'views/Tags';
+import Statistics from 'views/Statistics';
+import NoMatch from 'views/NoMatch';
 
 function App() {
   return (
       <Router>
-        <Wrapper>
-          <Main>
             <Switch>
             <Route path="/money">
               <Money />
@@ -37,29 +28,8 @@ function App() {
               <NoMatch />
             </Route>
           </Switch>
-          </Main>
-
-          <Nav/>
-
-        </Wrapper>
       </Router>
   );
 }
 
-function NoMatch(){
-  return (
-      <div>页面不存在</div>
-  )
-}
-function Statistics() {
-  return <h2>统计</h2>;
-}
-
-function Money() {
-  return <h2>记一笔</h2>;
-}
-
-function Tags() {
-  return <h2>标签</h2>;
-}
 export default App;
